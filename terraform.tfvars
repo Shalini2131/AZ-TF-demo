65b8b1108de160c5ad3e2bcdad643af7beeee7c8
AsishKunta <asishkunta@gmail.com>
2024-11-25 18:11:52 -0600

    removed secrets

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+azure_client_secret = var.azure_client_secret
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "azure_client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+}
+
 variable "subscription_id" {
   description = "The Azure subscription ID"
 }

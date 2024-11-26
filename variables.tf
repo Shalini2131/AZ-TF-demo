141c69b53a5f8250d77c30a05c7b6c48e5545874
AsishKunta <asishkunta@gmail.com>
2024-11-25 19:25:22 -0600

    #2 modified workflow file

--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "subscriptionId" {
   description = "The Azure subscription ID"
+  type        = string
 }
 
 variable "clientId" {
   description = "The Azure service principal client ID"
+  type        = string
 }
 
 variable "clientSecret" {
   description = "The Azure service principal client secret"
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "tenantId" {
   description = "The Azure tenant ID"
+  type        = string
 }

8e6391e0658172802adcfc7d40995c532b284f80
AsishKunta <asishkunta@gmail.com>
2024-11-27 23:35:22 -0600

    Setting up the remote backend - Bug fix#1

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-resource-group"
-    storage_account_name = "terraformstate2131 "
+    storage_account_name = "terraformstate2131"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
